0372af622d0699844a80283c6b726ca737893a6b
AdrixSC <adriana.be@outlook.com>
2018-04-03 18:19:26 -0500

    agregando maquetado

---
+++ js/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "Button.js"
+
+function App() {
+ return (<Button />)
+}
+
+ReactDOM.render(<App />, document.getElementById("container-react")
+);
