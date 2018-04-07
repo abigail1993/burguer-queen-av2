5f06e4ed0d72af605f874c9af36dae05bdbacab1
abigail1993 <alex.abi.roja@gmail.com>
2018-04-06 22:35:35 -0600

    con la carpeta de componentes

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import Row from "./Row"
+
+class Title extends Component {
+    render(){
+      return(<div className="App-background-titulo">
+          <h2 className="App-titulo"></h2>
+        </div>)
+  }
+ }
+ 
+ export default Title;
+ 
