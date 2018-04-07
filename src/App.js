666c28ef61d951d883bdbfa46b422d53003c7c5e
abigail1993 <alex.abi.roja@gmail.com>
2018-04-07 08:01:38 -0600

    ya trae router y si funciona

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import data from "./data.js";
-//import Button from "./Button.js"
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Button } from 'reactstrap';
 import './App.css';
-import Title from "./components/Title";
-import Table from "./components/Table";
-import Order from "./components/Order";
- 
+import Desayuno from "./components/Desayuno";
+import Comida from "./components/Comida";
+import Inicio from "./components/Inicio";
+
+//import Title from "./components/Title";
+//import Table from "./components/Table";
+//import Order from "./components/Order";
+import {Link, Switch, Route} from "react-router-dom";
+
 class App extends Component{
   constructor(){
     super()
@@ -26,12 +29,33 @@ class App extends Component{
     return (
       <section>
         <div className="App-container">
-          <div className="row">
+          <div className="row App-borde">
             <div className="col-md-6 offset-md-3 col-sm-6 offset-sm-3 m-b-30">
-               <h1 className="App-logo">{data.nameRestaurant}</h1>
+              <h1 className="App-logo">{data.nameRestaurant}</h1>
+              <Link className="" to="/Inicio">
+              Inicio
+              </Link>
+            </div>
+            <div className="col-md-5 ">
+              <Link className="App-estilo" to="/Desayuno">
+                Desayuno
+              </Link>
+            </div>
+            <div className="col-md-5 ">
+              <Link className="App-estilo" to="/Comida">
+                Comida
+              </Link>
             </div>
           </div>
         </div>
+      
+
+        <Switch>
+          <Route path="/Inicio" exact component={Inicio}></Route>
+          <Route path="/Desayuno" exact component={Desayuno}></Route>
+          <Route path="/Comida" exact component={Comida}></Route>
+
+        </Switch>
       </section>
    );
   }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from "react-router-dom";
+
 //import Button from "./Button.js"
 //import './App.css';
 
 
 
 
-ReactDOM.render(<App /> ,document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter> ,document.getElementById('root'));
 registerServiceWorker();
